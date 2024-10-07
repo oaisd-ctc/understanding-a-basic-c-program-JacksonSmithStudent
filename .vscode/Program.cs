2dad9649ec7a5c5b99ea490702df6b25bdc87fc2
JacksonSmithStudent <jasm076@student.oaisd.org>
2024-10-07 13:21:52 +0000

    fixed it

--- .vscode/Program.cs
+++ .vscode/Program.cs
@@ -8,13 +8,14 @@ class Program
         Console.WriteLine("Hello World!");
         Console.WriteLine("My name is jack smith.");
 
+        //Using Variables
         int age = 16;
         string greeting = "Hello World";
         string firstname = "Jack";
         string lastname = "Smith";
         double money = 15000;
 
-        Console.WriteLine(greating + "! " + "My name is " + firstname + " "
-        + lastname + ". I am " + age + "years old. I just spent $" + money + "on a dodge charger");
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " "
+        + lastname + ". I am " + age + " years old. I just spent $" + money + " on a dodge charger.");
     }
 }
