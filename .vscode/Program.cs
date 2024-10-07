ce556b3be4d6cd2807596d66b4fa035442c29947
JacksonSmithStudent <jasm076@student.oaisd.org>
2024-10-07 13:18:30 +0000

    add c#

---
+++ .vscode/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program 
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is jack smith.");
+
+        int age = 16;
+        string greeting = "Hello World";
+        string firstname = "Jack";
+        string lastname = "Smith";
+        double money = 15000;
+
+        Console.WriteLine(greating + "! " + "My name is " + firstname + " "
+        + lastname + ". I am " + age + "years old. I just spent $" + money + "on a dodge charger");
+    }
+}
